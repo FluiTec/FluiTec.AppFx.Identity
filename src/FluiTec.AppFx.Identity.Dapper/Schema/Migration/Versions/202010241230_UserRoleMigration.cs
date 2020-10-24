a2af27b11c06ced6df1b5a9b758cdddee8e1f114
Achim Schnell <a.schnell@wtschnell.de>
2020-10-24 12:37:13 +0200

    add userrolemigration

---
+++ src/FluiTec.AppFx.Identity.Dapper/Schema/Migration/Versions/202010241230_UserRoleMigration.cs
@@ -0,0 +1,65 @@
+﻿using FluiTec.AppFx.Data.Dapper.Migration;
+using FluiTec.AppFx.Identity.Data.Entities;
+using FluiTec.AppFx.Identity.Data.Schema;
+
+namespace FluiTec.AppFx.Identity.Dapper.Schema.Migration.Versions
+{
+    /// <summary>   A 202010241230 user role migration.</summary>
+    [DapperMigration(2020, 10, 24, 12, 30, "Achim Schnell")]
+    public class _202010241230_UserRoleMigration : FluentMigrator.Migration
+    {
+        /// <summary>   Name of the foreign key for user/userrole. </summary>
+        private const string ForeignKeyUserUserRole = "FK_UserRole_User";
+
+        /// <summary>   Name of the foreign key for user/userrole. </summary>
+        private const string ForeignKeyRoleUserRole = "FK_UserRole_Role";
+
+        /// <summary>   Collect the UP migration expressions.</summary>
+        public override void Up()
+        {
+            IfDatabase("sqlserver", "postgres")
+                .Create
+                .Table(SchemaGlobals.UserRoleTable)
+                .InSchema(SchemaGlobals.Schema)
+                .WithColumn(nameof(UserRoleEntity.Id)).AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn(nameof(UserRoleEntity.UserId)).AsGuid().NotNullable()
+                .ForeignKey(ForeignKeyUserUserRole, nameof(SchemaGlobals.UserTable), nameof(UserRoleEntity.UserId))
+                .WithColumn(nameof(UserRoleEntity.RoleId)).AsGuid().NotNullable()
+                .ForeignKey(ForeignKeyUserUserRole, nameof(SchemaGlobals.RoleTable), nameof(UserRoleEntity.RoleId));
+
+            IfDatabase("mysql")
+                .Create
+                .Table($"{SchemaGlobals.Schema}_{SchemaGlobals.UserRoleTable}")
+                .WithColumn(nameof(UserRoleEntity.Id)).AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn(nameof(UserRoleEntity.UserId)).AsGuid().NotNullable()
+                .WithColumn(nameof(UserRoleEntity.RoleId)).AsGuid().NotNullable();
+        }
+
+        /// <summary>   Collects the DOWN migration expressions.</summary>
+        public override void Down()
+        {
+            // remove foreign keys
+            IfDatabase("sqlserver", "postgres")
+                .Delete
+                .ForeignKey(ForeignKeyUserUserRole)
+                .OnTable(SchemaGlobals.UserLoginTable)
+                .InSchema(SchemaGlobals.Schema);
+
+            IfDatabase("sqlserver", "postgres")
+                .Delete
+                .ForeignKey(ForeignKeyRoleUserRole)
+                .OnTable(SchemaGlobals.UserLoginTable)
+                .InSchema(SchemaGlobals.Schema);
+
+            // remove tables
+            IfDatabase("sqlserver", "postgres")
+                .Delete
+                .Table(SchemaGlobals.UserRoleTable)
+                .InSchema(SchemaGlobals.Schema);
+
+            IfDatabase("mysql")
+                .Delete
+                .Table($"{SchemaGlobals.Schema}_{SchemaGlobals.UserRoleTable}");
+        }
+    }
+}
