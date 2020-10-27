c986029568800b313a3882aa51bfc0c55b7d3249
Achim Schnell <a.schnell@wtschnell.de>
2020-10-27 11:35:26 +0100

    update refs

--- src/samples/FluiTec.AppFx.Identity.Samples.DbSetup/Program.cs
+++ src/samples/FluiTec.AppFx.Identity.Samples.DbSetup/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Text.RegularExpressions;
 using FluiTec.AppFx.Identity.Data;
 using FluiTec.AppFx.Identity.Data.Entities;
+using FluiTec.AppFx.Options.Helpers;
 using FluiTec.AppFx.Options.Managers;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -19,7 +16,7 @@ namespace FluiTec.AppFx.Identity.Samples.DbSetup
         private static void Main(string[] args)
         {
             // load config from json
-            var path = GetApplicationRoot();
+            var path = DirectoryHelper.GetApplicationRoot();
             Console.WriteLine($"BasePath: {path}");
             var config = new ConfigurationBuilder()
                 .SetBasePath(path)
@@ -94,30 +91,6 @@ namespace FluiTec.AppFx.Identity.Samples.DbSetup
             });
 
             uow.Commit();
-        }
-
-        /// <summary>   Gets application root. </summary>
-        /// <exception cref="InvalidOperationException">
-        ///     Thrown when the requested operation is
-        ///     invalid.
-        /// </exception>
-        /// <returns>   The application root. </returns>
-        private static string GetApplicationRoot()
-        {
-            var exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
-
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                var appPathMatcher = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
-                var appRoot = appPathMatcher.Match(exePath ?? throw new InvalidOperationException()).Value;
-                return appRoot;
-            }
-            else
-            {
-                var appPathMatcher = new Regex(@"(?<!file)\/+[\S\s]*?(?=\/+bin)");
-                var appRoot = appPathMatcher.Match(exePath ?? throw new InvalidOperationException()).Value;
-                return appRoot;
-            }
         }
     }
 }
