92e4ab8d23bf0a5b7ad5e894f32abe7a0bf16329
Achim Schnell <a.schnell@wtschnell.de>
2022-05-09 12:45:52 +0200

    add version, remove all projects, update refs

--- src/FluiTec.AppFx.Identity/Data/IIdentityDataService.cs
+++
@@ -1,9 +0,0 @@
-﻿using FluiTec.AppFx.Data.DataServices;
-
-namespace FluiTec.AppFx.Identity.Data
-{
-    /// <summary>   Interface for identity data service. </summary>
-    public interface IIdentityDataService : IDataService<IIdentityUnitOfWork>
-    {
-    }
-}
